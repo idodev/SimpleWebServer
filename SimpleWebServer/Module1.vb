ba3cbbb84497af104590e15adaf4943a8e28fc5f
Toby Foord <toby@idodev.co.uk>
2016-03-22 10:59:20 +0000

    Initial Commit

---
+++ SimpleWebServer/Module1.vb
@@ -0,0 +1,38 @@
+﻿Module Module1
+
+    Sub Main(ByVal args As String())
+        If args.Length <> 2 Then
+            Console.WriteLine("Please provide a path and port")
+            LogEvent("Press any key to close...")
+            Console.ReadKey()
+            Exit Sub
+        End If
+
+
+        Dim RootPath As String = args(0)
+        Dim Port As Integer = args(1)
+
+        RootPath = IO.Path.Combine(Environment.CurrentDirectory, RootPath)
+        
+        Dim ws As New SimpleWebServer.WebServer(Port, RootPath, AddressOf LogEvent)
+        ws.StartServer()
+
+        LogEvent("Server Running at " + RootPath + "...", ConsoleColor.Green)
+        LogEvent("Press any key to stop...")
+        Console.ReadKey()
+        ws.StopServer()
+        ws = Nothing
+        LogEvent("Press any key to close...")
+        Console.ReadKey()
+
+    End Sub
+
+
+    Private Sub LogEvent(ByVal Text As String, Optional Color As ConsoleColor = Nothing)
+        If Color = Nothing Then Color = ConsoleColor.White
+        Console.ForegroundColor = Color
+        Console.WriteLine(Text)
+
+    End Sub
+
+End Module
---
+++ SimpleWebServer/SimpleWebServer.vb
@@ -0,0 +1,99 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Threading
+
+Namespace SimpleWebServer
+
+    Public Class WebServer
+        Private ReadOnly _listener As HttpListener
+        Private SourceFolder As IO.DirectoryInfo
+        Private Log As Action(Of String, ConsoleColor)
+
+
+        Public Sub New(ByVal Port As Integer, ByVal Source As String, ByVal l As Action(Of String, ConsoleColor))
+            _listener = New HttpListener
+            Try
+                Log = l
+                _listener.Prefixes.Add("http://localhost:" + Port.ToString + "/")
+                SourceFolder = New IO.DirectoryInfo(Source)
+
+            Catch
+
+            End Try
+        End Sub
+
+        Public Sub StartServer()
+            _listener.Start()
+            ThreadPool.QueueUserWorkItem( _
+                New WaitCallback( _
+                    Sub()
+                        Log("Webserver running...", ConsoleColor.Green)
+                        Try
+                            While _listener.IsListening
+                                ThreadPool.QueueUserWorkItem( _
+                                    New WaitCallback( _
+                                        Sub(ctx As HttpListenerContext)
+                                            ServeRequest(ctx.Request, ctx.Response)
+                                        End Sub
+                                    ), _listener.GetContext()
+                                )
+                            End While
+                        Catch
+
+                        End Try
+                    End Sub
+                )
+            )
+        End Sub
+
+        Public Function IsRunning() As Boolean
+            Return _listener.IsListening
+        End Function
+
+        Public Sub StopServer()
+            Log("Webserver stopped...", ConsoleColor.Red)
+            _listener.Stop()
+        End Sub
+
+
+        Private Sub ServeRequest(ByRef Request As HttpListenerRequest, ByRef Response As HttpListenerResponse)
+            Dim ThisFile As IO.FileInfo
+            Try
+                Dim RawUrl = Request.RawUrl
+                If RawUrl.EndsWith("/") Then
+                    RawUrl = RawUrl + "index.html"
+                End If
+                Dim FilePath As String = SourceFolder.FullName + RawUrl
+                ThisFile = New IO.FileInfo(FilePath)
+                Dim data() As Byte = New Byte() {}
+
+                If ThisFile.Exists Then
+                    Response.StatusCode = HttpStatusCode.OK
+                    data = File.ReadAllBytes(ThisFile.FullName)
+                    Log("200 - " + RawUrl, ConsoleColor.White)
+                Else
+                    Response.StatusCode = HttpStatusCode.NotFound
+                    Log("404 - " + RawUrl, ConsoleColor.Gray)
+                End If
+
+                Response.ContentLength64 = data.Length
+                Response.OutputStream.Write(data, 0, data.Length)
+
+            Catch ex As Exception
+                Response.StatusCode = HttpStatusCode.InternalServerError
+                Log("500 - " + Request.RawUrl, ConsoleColor.Red)
+            Finally
+                Response.OutputStream.Close()
+            End Try
+
+        End Sub
+
+        Private Function MapPath(ByVal Path As String) As String
+            Return SourceFolder.FullName + Path
+
+        End Function
+
+
+    End Class
+
+End Namespace
